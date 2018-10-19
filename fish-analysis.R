9a1da3c88ae61e53334ae7adcf0119c7b8c5825c
Christopher Fill <cfill2@unl.edu>
2018-10-19 11:33:51 -0500

    change length 300

--- fish-analysis.R
+++ fish-analysis.R
@@ -4,8 +4,8 @@ library(tidyverse)
 source("R/2018-10-12-Themes.R")
 fishData <- read_csv("https://raw.githubusercontent.com/cfill2/gittutorial/master/data/Gaeta_etal_CLC_data.csv")
 
-#fishData <- read_csv("data/")
+#fishData <- read_csv("data/Gaeta_etal")
 
 fishData %>% 
-  mutate(length_cat = case_when(length >= 200 ~ "big",
-                                length < 200 ~ "small")) -> fishData
+  mutate(length_cat = case_when(length >= 300 ~ "big",
+                                length < 300 ~ "small")) -> fishData
